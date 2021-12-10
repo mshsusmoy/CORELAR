73f3e815004dcd27bfdb9c55899e1fc5ddde5b1e
mshsusmoy <mshsusmoy@gmail.com>
2021-12-10 20:11:04 +0600

    API GET+ Angular Client Initialization

---
+++ API/client/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CORELAR APP';
+  sub_title = 'Developed By Susmoy'
+}

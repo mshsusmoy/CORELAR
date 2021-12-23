b2fb1a16616e631e68549785dd078ddd4d513908
mshsusmoy <mshsusmoy@gmail.com>
2021-12-23 21:24:52 +0600

    Register Page Validation

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -22,10 +22,20 @@ export class RegisterComponent implements OnInit {
 
   initializeForm(){
     this.registerForm = new FormGroup({
-      userName: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl()
+      userName: new FormControl('Hello',Validators.required),
+      password: new FormControl('',[Validators.required, Validators.minLength(4),Validators.maxLength(8)]),
+      confirmPassword: new FormControl('',[Validators.required, this.matchValues('password')])
     })
+    this.registerForm.controls.password.valueChanges.subscribe(() =>{
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn{
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value 
+        ? null : {isMatching: true} 
+    }
   }
 
   register(){

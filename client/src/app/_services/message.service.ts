b5765e8ad429be8d86dd3d9b282ff324a8ef2c9e
mshsusmoy <mshsusmoy@gmail.com>
2022-01-01 17:08:30 +0600

    Messages Setup Phase-2

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -17,6 +17,8 @@ export class MessageService {
     params = params.append('Container', container);
     return getPaginatedResults<Message[]>(this.baseUrl + 'messages', params, this.http);
   }
-
+  getMessageThread(userName: string){
+    return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + userName);
+  }
   
 }
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -29,8 +29,10 @@ export class MessagesComponent implements OnInit {
   }
 
   pageChanged(event: any){
-    this.pageNumber = event.page;
-    this.loadMessages();
+    if(this.pageNumber !== event.page){
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
   }
 
 }

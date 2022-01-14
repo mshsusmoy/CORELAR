f267911f007b4b2f0a8efdba2ff2bd62549ef371
mshsusmoy <mshsusmoy@gmail.com>
2022-01-15 00:02:22 +0600

    Status DB Added

---
+++ API/Data/Migrations/20220114175906_StatusAdded.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace API.Data.Migrations
+{
+    public partial class StatusAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StatusOwnerName = table.Column<string>(type: "text", nullable: true),
+                    StatusOwnerId = table.Column<int>(type: "integer", nullable: true),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    StatusPostTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    StatusDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Statuses_AspNetUsers_StatusOwnerId",
+                        column: x => x.StatusOwnerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Statuses_StatusOwnerId",
+                table: "Statuses",
+                column: "StatusOwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Statuses");
+        }
+    }
+}
